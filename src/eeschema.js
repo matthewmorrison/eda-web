7e0b5a12b339e506eca5f78a7d4392fbdb8f4ef6
Matthew Morrison <matthewemorrison@gmail.com>
2014-08-05 08:24:57 -0700

    Update eeschema.js

--- src/eeschema.js
+++ src/eeschema.js
@@ -18,7 +18,7 @@ function EeSchema(container) {
 	this.canvasContainer = $('<div class="eeschema-canvas-container" />');
 	this.coords = $('<div class="eeschema-coords" >Coords</div>');
 	this.canvasTouch = $('<div class="eeschema-canvas-touch" >');
-	this.target = $('<div class="debug" >4</div>');
+	this.target = $('<div class="debug" >5</div>');
 	this.scale = $('<div class="scale" >1</div>');
 	this.deltas = $('<div class="coords" >0, 0</div>');
 	this.redrawTime = $('<div class="redraw-time" >redraw</div>');
