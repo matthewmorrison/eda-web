9ad1a7bc8fb3ef7af3bc770ad66dc441712b147c
Matthew Morrison <matthewemorrison@gmail.com>
2014-08-11 10:04:43 -0700

    Update eeschema-component.js
    Fixed polygon rendering.

--- src/eeschema-component.js
+++ src/eeschema-component.js
@@ -107,14 +107,11 @@ ComponentDefinition.prototype.parseDrawField = function(props) {
 			'convert': props[3],
 			'thickness': props[4], 
 			'points': [
-				[ props[5], props[6] ],
-				[ props[7], props[8] ]
 			]
 		};
 		
-		for(var i = 9; i < props.length -1; i+=2) {
-			p.points.push([ props[i], props[i+1] ]);
-		}
+		for(var i = 5; i < props.length -1; i+=2)
+			p.points.push([ parseInt(props[i]), -parseInt(props[i+1]) ]);
 		
 		this.elements.push(p);
 	}
@@ -122,9 +119,9 @@ ComponentDefinition.prototype.parseDrawField = function(props) {
 		this.elements.push({
 			'type': type, 
 			'x0': parseInt(props[1]),
-			'y0': parseInt(props[2]), 
+			'y0': -parseInt(props[2]), 
 			'x1': parseInt(props[3]), 
-			'y1': parseInt(props[4]),
+			'y1': -parseInt(props[4]),
 			'unit': props[5], 
 			'convert': props[6],
 			'thickness': parseInt(props[7]),
@@ -134,8 +131,8 @@ ComponentDefinition.prototype.parseDrawField = function(props) {
 	else if(type == 'C') {
 		this.elements.push({
 			'type': type, 
-			'x0': props[1],
-			'y0': props[2], 
+			'x0': parseInt(props[1]),
+			'y0': -parseInt(props[2]), 
 			'radius': props[3], 
 			'unit': props[4],
 			'convert': props[5], 
@@ -146,8 +143,8 @@ ComponentDefinition.prototype.parseDrawField = function(props) {
 	else if(type == 'A') {
 		this.elements.push({
 			'type': type, 
-			'x0': props[1],
-			'y0': props[2], 
+			'x0': parseInt(props[1]),
+			'y0': -parseInt(props[2]), 
 			'radius': props[3], 
 			'start': props[4],
 			'end': props[5], 
@@ -155,18 +152,18 @@ ComponentDefinition.prototype.parseDrawField = function(props) {
 			'convert': props[7],
 			'thickness': props[8],
 			'cc': props[9].trim(),
-			'sX': props[10],
-			'sY': props[11],
-			'eX': props[12],
-			'eY': props[13]
+			'sX': -parseInt(props[10]),
+			'sY': -parseInt(props[11]),
+			'eX': -parseInt(props[12]),
+			'eY': -parseInt(props[13])
 		});
 	}
 	else if(type == 'T') {
 		this.elements.push({
 			'type': type, 
 			'orientation': props[1],
-			'x0': props[2], 
-			'y0': props[3], 
+			'x0': -parseInt(props[2]), 
+			'y0': -parseInt(props[3]), 
 			'dimension': props[4],
 			'unit': props[5], 
 			'convert': props[6],
@@ -178,9 +175,9 @@ ComponentDefinition.prototype.parseDrawField = function(props) {
 			'type': props[0],
 			'name': props[1],
 			'number': props[2],
-			'x0': props[3],
-			'y0': props[4],
-			'length': props[5],
+			'x0': parseInt(props[3]),
+			'y0': -parseInt(props[4]),
+			'length': parseInt(props[5]),
 			'orientation': props[6],
 			'snum': props[7],
 			'snom': props[8],
@@ -230,102 +227,68 @@ ComponentDefinition.prototype.Create = function(x, y) {
 		var el = this.elements[i];
 		
 		if(el.type == 'P') {
-			var path = 'M ' + el.points[0][0] + ' ' + el.points[0][1] + 
-				' L ' + el.points[1][0] + ' ' + el.points[1][1];
+			var p = this.CreatePolygon(el);
 			
-			for(var j = 2; j < el.points.length; j++) {
-				path += ' L ' + el.points[j][0] + ' ' + el.points[j][1];
-			}
-			
-			path += ' Z';
-			
-			var path = new fabric.Path(path);
-			path.set({ fill: 'transparent', stroke: 'black', strokeWidth: this.widthFactor, originX: 0, originY: 0 });
-			path.hasBorders = path.hasControls = false;
-			
-			var poly = new fabric.Group([path], {
-				originX: 0,
-				originY: 0
-			});
-			
-			group.add(poly);
+			group.add(p);
 		}
 		else if(el.type == 'X') {
 			var visible = true;
 			
 			if(typeof el.shape != 'undefined' && el.shape.length > 0)
 				visible = el.shape[0] != 'N';
-		
-			var circle = new fabric.Circle({ 
-				radius: pinRadius, 
-				fill: 'white', 
-				stroke: 'black', 
-				strokeWidth: 2, 
-				originX: center.left, 
-				originY: center.top,
+			
+			var angle = 0;
+			var tAngle = 0;
+			var tOriginX = 'left';
+			
+			if(el.orientation == 'D') {
+				angle = 90;
+				tAngle = 180;
+				tOriginX = 'right';
+			}
+			else if(el.orientation == 'L') {
+				angle = 180;
+				tAngle = 180;
+				tOriginX = 'right';
+			}
+			else if(el.orientation == 'U') {
+				angle = 270;
+			}
+			
+			var circle = new fabric.Circle({
+				radius: pinRadius,
+				fill: 'transparent',
+				stroke: 'black',
+				strokeWidth: 2,
+				originX: 'center',
+				originY: 'center',
 				left: el.x0,
 				top: el.y0,
 				visible: visible
+			});
+			
+			var line = new fabric.Line([ pinRadius, 0, el.length - pinRadius, 0 ], {
+				originX: 'center',
+				originY: 'center',
+				stroke: 'black',
+				strokeWidth: 2
 			});
 			
 			var name = new fabric.Text(el.name, {
-				originX: center.left,
-				originY: center.top,
-				left: el.x0,
-				top: el.y0,
-				visible: visible
+				originX: tOriginX,
+				originY: 'center',
+				left: el.length,
+				angle: tAngle
 			});
 			
-			var x0 = parseInt(el.x0);
-			var y0 = parseInt(el.y0);
-			var x1 = x0;
-			var y1 = y0;
-			var length = parseInt(el.length);
-			var half = width/2;
-			
-			if(el.orientation == 'D') {
-				y0 -= length;
-				x0 -= half;
-				x1 -= half;
-				name.angle -= 90;
-				name.top = parseInt(name.getTop()) - (length + name.getWidth()/2 + pinRadius);
-			}
-			else if(el.orientation == 'R') {
-				x1 += length;
-				y0 -= half;
-				y1 -= half;
-				name.left = parseInt(name.getLeft()) + (length + name.getWidth()/2 + pinRadius);
-			}
-			else if(el.orientation == 'L') {
-				x0 -= length;
-				y0 -= half;
-				y1 -= half;
-				name.left = parseInt(name.getLeft()) - (length + name.getWidth()/2 +pinRadius);
-			}
-			else {
-				y1 += length;
-				x0 -= half;
-				x1 -= half;
-				name.angle -= 90;
-				name.top = parseInt(name.getTop()) + (length + name.getWidth()/2 + pinRadius);
-			}
-			
-			var line = new fabric.Line([ x0, y0, x1, y1 ], {
-				stroke: 'black',
-				strokeWidth: width,
-				left: x0,
-				top: y0,
-				visible: visible
-			});				
-			
-			name.hasBorders = name.hasControls = false;
-			line.hasBorders = line.hasControls = false;
-			circle.hasBorders = circle.hasControls = false;
-			
-			var pin = new fabric.Group([name, line, circle], {
-				originX: 0,
-				originY: 0
+			var pin = new fabric.Group([circle], {
+				originX: 'center',
+				originY: 'center',
+				angle: angle
 			});
+			
+			pin.add(line);
+			pin.add(name);
 			
 			group.add(pin);
 		}
@@ -400,19 +363,37 @@ ComponentDefinition.prototype.Create = function(x, y) {
 			    strokeWidth: this.widthFactor,
 				left: el.x0,
 				top: el.y0,
-			    originX: center.left, 
-			    originY: center.top,
+			    originX: 'center', 
+			    originY: 'center',
                 fill: el.cc == 'N' ? 'transparent' : 'black'
 			});
 			
-			var arc = new fabric.Group([path], {
-				originX: 0,
-				originY: 0
-			});
-			
-			group.add(arc);
+			group.add(path);
 		}
 	}
 	
 	return group;
+}
+
+ComponentDefinition.prototype.CreatePolygon = function(el) {			
+			var lines = new fabric.Group([], {
+				originX: 'center',
+				originY: 'center'
+			});
+			
+			for(var j = 0; j < el.points.length - 1; j++) {
+				var x0 = el.points[j][0];
+				var y0 = el.points[j][1];
+				var x1 = el.points[j+1][0];
+				var y1 = el.points[j+1][1];
+				
+				lines.add(new fabric.Line([ x0, y0, x1, y1 ], {
+					originX: 'center',
+					originY: 'center',
+					strokeWidth: this.widthFactor,
+					stroke: 'black'
+				}));				
+			}
+			
+			return lines;
 }
